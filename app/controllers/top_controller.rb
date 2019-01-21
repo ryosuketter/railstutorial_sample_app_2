0abaee9c88184e32dc8e54d59e32a725b3ee1766
ryosuketter <r.shimizu.yale@gmail.com>
2019-01-21 09:02:24 +0900

    こみっと

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,7 @@
 class TopController < ApplicationController
   def index
-    @authos = Author.all.includes(:books) # 28.9 .7ms / 2sql
-    # @authos = Author.all # 40.7ms / 5sql
+    @authos = Author.all.includes(:books) # 30.0ms / 2sql
+    # @authos = Author.all # 38.8ms / 9sql
     # @books = Book.all.includes(:author) #31.9 ms  / 2sql
     # @books = Book.all #76.6ms / 5sql
   end
