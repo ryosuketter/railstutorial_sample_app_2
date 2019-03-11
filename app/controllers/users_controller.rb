e2c6ca2bc800d91ec1fb3ee3696dc89020a9073f
ryosuketter <r.shimizu.yale@gmail.com>
2019-03-11 22:53:24 +0900

    やったこと 第10章 「10.2.3 フレンドリーフォワーディング まで」 次回は URL https://railstutorial.jp/chapters/updating_and_deleting_users?version=5.1#sec-friendly_forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -25,7 +28,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # 
+      flash[:success] = 'Profile updated'
+      redirect_to @user
     else
       render 'edit'
     end
@@ -35,5 +39,21 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    # beforeアクション
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
