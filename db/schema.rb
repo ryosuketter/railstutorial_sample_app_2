6e847acaf1e5b4f5b29fcba328e6223b057706b4
ryosuketter <r.shimizu.yale@gmail.com>
2019-03-28 11:38:53 +0900

    10.4.1 管理ユーザー

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_133902) do
+ActiveRecord::Schema.define(version: 2019_03_28_021755) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_02_26_133902) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
