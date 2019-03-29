64863b9ec03166ff08f46c9c6861d50704f6651a
ryosuketter <r.shimizu.yale@gmail.com>
2019-03-30 05:48:20 +0900

    やったこと 第13章 「13.1.2 Micropostのバリデーション まで」 次回は URL https://railstutorial.jp/chapters/user_microposts?version=5.1#sec-micropost_validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_28_121807) do
+ActiveRecord::Schema.define(version: 2019_03_29_201928) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 2019_03_28_121807) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
